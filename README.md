f4e0820345a9e46887e7607ee1c4ce8a35169980
wk-j <somnuk.wk@gmail.com>
2019-03-16 01:44:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Issue Exporter
+
+[![NuGet](https://img.shields.io/nuget/v/wk.IssueExporter.svg)](https://www.nuget.org/packages/wk.IssueExporter)
+
+## Installation
+
+```bash
+dotnet tool install -g wk.IssueExporter
+```
+
+## Usage
+
+```bash
+wk-issue-exporter --repo bcircle/practika-crm --token $GITHUB_TOKEN
+```
---
+++ src/IssueExporter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Octokit;
+using System.Linq;
+using System.IO;
+using CsvHelper;
+
+namespace IssueExporter {
+    class MyIssue {
+        public string CreatedAt { set; get; }
+        public string Title { set; get; }
+        public ItemState State { set; get; }
+    }
+
+    class Program {
+        static async Task Main(
+                string repo = "wk-j/issue-exporter",
+                string token = ""
+            ) {
+
+            var gh = new GitHubClient(new ProductHeaderValue("wk-app"));
+            gh.Credentials = new Credentials(token);
+
+            var tokens = repo.Split("/");
+            var org = tokens[0];
+            var repository = tokens[1];
+
+            var issues = await gh.Issue.GetAllForRepository(org, repository, new RepositoryIssueRequest {
+                State = ItemStateFilter.All
+            });
+
+            var data = issues.Select(x => new MyIssue {
+                CreatedAt = x.CreatedAt.DateTime.ToString("dd/MM/yyyy"),
+                Title = x.Title,
+                State = x.State.Value
+            }).OrderBy(x => x.CreatedAt);
+
+            var fileName = repo.Replace("/", ".") + ".csv";
+
+            using (var writer = new StreamWriter(fileName))
+            using (var csv = new CsvWriter(writer)) {
+                csv.WriteRecords(data);
+            }
+        }
+    }
+}
